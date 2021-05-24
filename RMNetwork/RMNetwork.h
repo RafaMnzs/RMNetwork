bd7dc9b1ea9c45e24672e0021860cec436e8c12a
Rafael Menezes <rafael.r.m@outlook.com>
2021-05-24 20:02:09 -0300

    Initial Commit

---
+++ RMNetwork/RMNetwork.h
@@ -0,0 +1,18 @@
+//
+//  RMNetwork.h
+//  RMNetwork
+//
+//  Created by Rafael Menezes on 24/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RMNetwork.
+FOUNDATION_EXPORT double RMNetworkVersionNumber;
+
+//! Project version string for RMNetwork.
+FOUNDATION_EXPORT const unsigned char RMNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMNetwork/PublicHeader.h>
+
+
